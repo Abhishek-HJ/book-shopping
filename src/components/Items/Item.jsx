1190ee9f0abd768336b908754a821a7fa05f19d5
Abhishek-HJ <abhishek.hj35@gmail.com>
2024-07-08 22:34:22 +0530

    a

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -5,10 +5,10 @@ const Item = (props) => {
     <div className='item'>
         <img src={props.image} style={{width:'300px', height:'300px'}}></img>
         <p>{props.name}</p>
-        <dic className="item-prices">
+        <div className="item-prices">
             <div className="item-price-new">{props.new_price}</div>
             <div className="item-price-old">{props.old_price}</div>
-        </dic></div>
+        </div></div>
   )
 }
 
